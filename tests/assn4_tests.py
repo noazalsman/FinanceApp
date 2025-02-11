cab9e66d1e79aa708594595290a3a2b13fb7adb3
Noa Zalsman <noaza@wix.com>
2025-02-11 11:41:53 +0200

    add query job

--- tests/assn4_tests.py
+++ tests/assn4_tests.py
@@ -1,67 +1,20 @@
 import pytest
 import requests
+import os
 
-BASE_URL = "http://localhost:5001"  # Replace with your actual base URL
+BASE_URL = "http://localhost:5001"
 
 @pytest.fixture
 def stock_details():
-    return {
-        "stock1": {
-            "name": "NVIDIA Corporation",
-            "symbol": "NVDA",
-            "purchase price": 134.66,
-            "purchase date": "18-06-2024",
-            "shares": 7
-        },
-        "stock2": {
-            "name": "Apple Inc.",
-            "symbol": "AAPL",
-            "purchase price": 183.63,
-            "purchase date": "22-02-2024",
-            "shares": 19
-        },
-        "stock3": {
-            "name": "Alphabet Inc.",
-            "symbol": "GOOG",
-            "purchase price": 140.12,
-            "purchase date": "24-10-2024",
-            "shares": 14
-        },
-        "stock4": {
-            "name": "Tesla, Inc.",
-            "symbol": "TSLA",
-            "purchase price": 194.58,
-            "purchase date": "28-11-2022",
-            "shares": 32
-        },
-        "stock5": {
-            "name": "Microsoft Corporation",
-            "symbol": "MSFT",
-            "purchase price": 420.55,
-            "purchase date": "09-02-2024",
-            "shares": 35
-        },
-        "stock6": {
-            "name": "Intel Corporation",
-            "symbol": "INTC",
-            "purchase price": 19.15,
-            "purchase date": "13-01-2025",
-            "shares": 10
-        },
-        "stock7": {
-            "name": "Amazon.com, Inc.",
-            "purchase price": 134.66,
-            "purchase date": "18-06-2024",
-            "shares": 7
-        },  # Missing "symbol"
-        "stock8": {
-            "name": "Amazon.com, Inc.",
-            "symbol": "AMZN",
-            "purchase price": 134.66,
-            "purchase date": "Tuesday, June 18, 2024",
-            "shares": 7
-        }  # Incorrect date format
-    }
+    stock_data = {}
+    stock_dir = "tests/stock-objects"
+
+    for file in os.listdir(stock_dir):
+        if file.endswith(".json"):
+            with open(os.path.join(stock_dir, file), "r") as f:
+                stock_data[file.replace(".json", "")] = json.load(f)
+
+    return stock_data
 
 @pytest.fixture(scope="session")
 def created_stock_ids():
